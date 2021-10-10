86cbb4b83785c3396808730d3b6eaf1962b252f0
AdityaCipta <77426245+AdityaCipta@users.noreply.github.com>
2021-10-10 22:16:33 +0700

    Problem 8 - Full Project
    This is a full project from problems 1 to 8

---
+++ 10 Problem Solving Challenge/Assets/Scripts/Problem5.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Problem5 : MonoBehaviour
+{
+    private Rigidbody2D rigidbody2d;
+    public float speed = 10.0f;
+    Vector2 LastClicked;
+
+    [SerializeField]
+    bool Moving;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            LastClicked = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Moving = true;
+        }
+
+        if (Moving && (Vector2)transform.position != LastClicked)
+        {
+            float onward = speed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, LastClicked, onward);
+        }
+        else
+        {
+            Moving = false;   
+        }
+    }
+}
---
+++ 10 Problem Solving Challenge/Assets/Scripts/Problem6.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Problem6 : MonoBehaviour
+{
+    public GameObject Square;
+    float randomPositionX;
+    float randomPositionY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        float countSpawn = Random.Range(5,25);
+        for (int i = 0; i < countSpawn; i++)
+        {
+            randomPositionX = Random.Range(-12f,12f);
+            randomPositionY = Random.Range(-6f,6f);
+            transform.position = new Vector2(randomPositionX, randomPositionY);
+            Instantiate(Square, transform.position, Quaternion.identity);
+        }
+        Invoke("Start",3);
+    }
+}
