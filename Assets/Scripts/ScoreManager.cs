7c768ed0e6d2fa24640be0ff7c332b262d385b81
AdityaCipta <77426245+AdityaCipta@users.noreply.github.com>
2021-10-10 22:09:06 +0700

    Problem 7

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public Text ScoreValue;
+    public static int Score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateScore();
+    }
+
+    private void UpdateScore()
+    {
+        ScoreValue.text = ""+Score;
+    }
+}
